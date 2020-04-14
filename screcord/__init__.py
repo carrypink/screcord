a502c4aa0a25e9447c01c19fb25a9c5098447f77
ssfanli <freedomlidi@163.com>
2020-04-14 22:19:49 +0800

    prepare pypi package

---
+++ screcord/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__PROJECT_NAME__ = r"screcord"
+__AUTHOR__ = r"ssfanli"
+__AUTHOR_EMAIL__ = r"freedomlidi@163.com"
+__LICENSE__ = r"MIT"
+__URL__ = r"https://github.com/ssfanli/screcord.git"
+__VERSION__ = r"0.0.1"
+__DESCRIPTION__ = r"A python wrapper for Android/iOS screen recording."
+
---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+from screcord import (
+    __AUTHOR__,
+    __AUTHOR_EMAIL__,
+    __URL__,
+    __LICENSE__,
+    __VERSION__,
+    __PROJECT_NAME__,
+    __DESCRIPTION__,
+)
+
+setup(
+    name=__PROJECT_NAME__,
+    version=__VERSION__,
+    description=__DESCRIPTION__,
+    author=__AUTHOR__,
+    author_email=__AUTHOR_EMAIL__,
+    url=__URL__,
+    packages=find_packages(),
+    include_package_data=True,
+    license=__LICENSE__,
+    python_requires=">=3.6",
+    install_requires=["loguru"]
+)
