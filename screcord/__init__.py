4689a1a53fa4b8cae2ba11161f3dc671a19fc14a
ssfanli <freedomlidi@163.com>
2020-05-24 21:41:20 +0800

    update version

--- screcord/__init__.py
+++ screcord/__init__.py
@@ -6,7 +6,7 @@ __AUTHOR__ = r"ssfanli"
 __AUTHOR_EMAIL__ = r"freedomlidi@163.com"
 __LICENSE__ = r"MIT"
 __URL__ = r"https://github.com/ssfanli/screcord.git"
-__VERSION__ = r"0.0.5"
+__VERSION__ = r"0.0.6"
 __DESCRIPTION__ = r"A python wrapper for Android/iOS screen recording."
 
 import typing
